d0573111272e0102859d4285d400be981803334c
scscodes <54192292+scscodes@users.noreply.github.com>
2025-06-09 09:49:30 -0400

    Remove unused onSettingsChange prop

--- this-or-that/src/app/games/numbers/page.tsx
+++ this-or-that/src/app/games/numbers/page.tsx
@@ -4,14 +4,13 @@ import GameBoard from "@/components/GameBoard";
 import { useGame } from "@/hooks/useGame";
 
 export default function NumbersGame() {
-  const { questions, handleSettingsChange } = useGame("numbers");
+  const { questions } = useGame("numbers");
   
   return (
     <GameBoard 
       title="Numbers Game" 
       questions={questions}
       gameType="numbers"
-      onSettingsChange={handleSettingsChange}
     />
   );
 } 
